4c17d9577813060d99c14282a6a1cf93952c2d9c
Iam Melo <iammelo@Iams-MacBook-Air.local>
2026-05-08 13:29:44 -0300

    feat: adds data analysis notebook

--- src/extract.py
+++ src/extract.py
@@ -3,14 +3,18 @@ import json
 from pathlib import Path
 import logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-API_KEY = '2f2bb4f748f9ef9fbecade6cff6228ae'
-url=f'https://api.openweathermap.org/data/2.5/weather?q=Sao Paulo,BR&units=metric&appid={API_KEY}'
+
+
 def extract_weather_data(url:str) -> list:
     response = requests.get(url)
     data = response.json()
 
     if response.status_code != 200:
         logging.error("Request Error")
+        return []
+    
+    if not data:
+        logging.warn("No data found")
         return []
     
     output_path = 'data/weather_data.json'
